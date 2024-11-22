6f336bfa3ca03690db5c0f6717d6b76f8b861464
Udayakeerthi0123 <114857285+Udayakeerthi0123@users.noreply.github.com>
2024-11-22 19:46:15 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,322 @@
+from google.colab import drive
+drive.mount('/content/drive')
+pip install split-folders
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as npa
+import pandas as pd
+import random
+import cv2
+import os
+import PIL
+import pathlib
+import splitfolders
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
+# Import ImageDataGenerator from the correct module in TensorFlow 2.x
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras.applications.vgg16 import VGG16
+#dataset loading and splitting
+base_ds = '/content/drive/MyDrive/rice'
+base_ds = pathlib.Path(base_ds)
+splitfolders.ratio(base_ds, output='/content/drive/MyDrive/imgs', seed=123, ratio=(.7,.15,.15), group_prefix=None)
+arborio = [fn for fn in os.listdir(f'{base_ds}/Arborio') if fn.endswith('.jpg')]
+bastmati = [fn for fn in os.listdir(f'{base_ds}/Basmati') if fn.endswith('.jpg')]
+ipsala = [fn for fn in os.listdir(f'{base_ds}/Ipsala') if fn.endswith('.jpg')]
+jasmine = [fn for fn in os.listdir(f'{base_ds}/Jasmine') if fn.endswith('.jpg')]
+karacadag = [fn for fn in os.listdir(f'{base_ds}/Karacadag') if fn.endswith('.jpg')]
+rice = [arborio, bastmati, ipsala, jasmine, karacadag]
+rice_classes = []
+for i in os.listdir('/content/drive/MyDrive/imgs/train'):
+    rice_classes+=[i]
+rice_classes.sort()
+#dataset Exploration
+image_count = len(list(base_ds.glob('*/*.jpg')))
+print(f'Total images: {image_count}')
+print(f'Total number of classes: {len(rice_classes)}')
+count = 0
+rice_count = []
+for x in rice_classes:
+  print(f'Total {x} images: {len(rice[count])}')
+  rice_count.append(len(rice[count]))
+  count += 1
+
+sns.set_style('darkgrid')
+sns.barplot(x=rice_classes, y=rice_count, palette="Blues_d")
+plt.show()
+#sample image as input
+sample_img = cv2.imread('/content/drive/MyDrive/imgs/test/Ipsala/Ipsala (4).jpg')
+plt.imshow(sample_img)
+print(f'Image dimensions: {sample_img.shape}')
+def load_random_img(dir, label):
+  plt.figure(figsize=(10,10))
+  i=0
+  for label in rice_classes:
+    i+=1
+    plt.subplot(1, 5, i)
+    file = random.choice(os.listdir(f'{dir}/{label}'))
+    image_path = os.path.join(f'{dir}/{label}', file)
+    img=cv2.imread(image_path)
+    plt.title(label)
+    plt.imshow(img)
+    plt.grid(None)
+    plt.axis('off')
+for i in range(3):
+  load_random_img(base_ds, rice_classes)
+batch_size = 128
+img_height, img_width = 175, 175
+input_shape = (img_height, img_width, 3)
+#data pre-processing
+datagen = ImageDataGenerator(rescale=1./255)
+train_ds = datagen.flow_from_directory(
+    '/content/drive/MyDrive/imgs/train',
+    target_size = (img_height, img_width),
+    batch_size = batch_size,
+    subset = "training",
+    class_mode='categorical')
+
+test_ds = datagen.flow_from_directory(
+    '/content/drive/MyDrive/imgs/test',
+    target_size = (img_height, img_width),
+    batch_size = batch_size,
+    class_mode='categorical',
+    shuffle=False)
+
+val_ds = datagen.flow_from_directory(
+    '/content/drive/MyDrive/imgs/val',
+    target_size = (img_height, img_width),
+    batch_size = batch_size,
+    class_mode='categorical',
+    shuffle=False)
+def plot_train_history(history):
+    plt.figure(figsize=(15,5))
+    plt.subplot(1,2,1)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('Model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+
+    plt.subplot(1,2,2)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+#Vanilla CNN model
+model_vanilla = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32,(3,3), activation='relu', input_shape=input_shape),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(32,(3,3),activation='relu',padding='same'),
+    tf.keras.layers.BatchNormalization(axis = 3),
+    tf.keras.layers.MaxPooling2D(pool_size=(2,2),padding='same'),
+    tf.keras.layers.Dropout(0.3),
+
+    tf.keras.layers.Conv2D(64,(3,3),activation='relu',padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(64,(3,3),activation='relu',padding='same'),
+    tf.keras.layers.BatchNormalization(axis = 3),
+    tf.keras.layers.MaxPooling2D(pool_size=(2,2),padding='same'),
+    tf.keras.layers.Dropout(0.3),
+
+    tf.keras.layers.Conv2D(128,(3,3),activation='relu',padding='same'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Conv2D(128,(3,3),activation='relu',padding='same'),
+    tf.keras.layers.BatchNormalization(axis = 3),
+    tf.keras.layers.MaxPooling2D(pool_size=(2,2),padding='same'),
+    tf.keras.layers.Dropout(0.5),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.BatchNormalization(),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Dense(5, activation='softmax')
+])
+model_vanilla.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model_vanilla.summary()
+#callbacks
+models_dir = "saved_models"
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
+# Change the file extension to '.keras'
+checkpointer = ModelCheckpoint(filepath='saved_models/model_vanilla.keras', # Use '.keras' extension
+                               monitor='val_accuracy', mode='max',
+                               verbose=1, save_best_only=True)
+early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=3)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,patience=2, min_lr=0.001)
+callbacks=[early_stopping, reduce_lr, checkpointer]
+#callbacks
+models_dir = "saved_models"
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
+# Change the file extension to .keras
+checkpointer = ModelCheckpoint(filepath='saved_models/model_vanilla.keras',
+                               monitor='val_accuracy', mode='max',
+                               verbose=1, save_best_only=True)
+early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=3)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,patience=2, min_lr=0.001)
+callbacks=[early_stopping, reduce_lr, checkpointer]
+history1 = model_vanilla.fit(train_ds, epochs = 10, validation_data = val_ds, callbacks=callbacks)
+model_vanilla.load_weights('saved_models/model_vanilla.keras')
+plot_train_history(history1)
+from sklearn.metrics import classification_report, confusion_matrix
+
+Y_pred = model_vanilla.predict(test_ds)
+y_pred = np.argmax(Y_pred, axis=1)
+confusion_mtx = confusion_matrix(y_pred, test_ds.classes)
+f,ax = plt.subplots(figsize=(12, 12))
+sns.heatmap(confusion_mtx, annot=True,
+            linewidths=0.01,
+            linecolor="yellow",
+            fmt= '.1f',ax=ax,)
+sns.color_palette("rocket", as_cmap=True)
+
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+ax.xaxis.set_ticklabels(test_ds.class_indices)
+ax.yaxis.set_ticklabels(rice_classes)
+plt.title("Confusion Matrix")
+plt.show()
+report1 = classification_report(test_ds.classes, y_pred, target_names=rice_classes, output_dict=True)
+df1 = pd.DataFrame(report1).transpose()
+df1
+#VGG16
+vgg16 = VGG16(weights="imagenet", include_top=False, input_shape=input_shape)
+vgg16.trainable = False
+inputs = tf.keras.Input(input_shape)
+x = vgg16(inputs, training=False)
+x = tf.keras.layers.GlobalAveragePooling2D()(x)
+x = tf.keras.layers.Dense(1024, activation='relu')(x)
+x = tf.keras.layers.Dense(5, activation='softmax')(x)
+model_vgg16 = tf.keras.Model(inputs, x)
+model_vgg16.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model_vgg16.summary()
+checkpointer = ModelCheckpoint(filepath='saved_models/model_vgg16.keras', # Change file extension to '.keras'
+                               monitor='val_accuracy', mode='max',
+                               verbose=1, save_best_only=True)
+early_stopping = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=3)
+reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2,patience=2, min_lr=0.001)
+callbacks=[early_stopping, reduce_lr, checkpointer]
+history2 = model_vgg16.fit(train_ds, epochs = 10, validation_data = val_ds, callbacks=callbacks)
+model_vgg16.load_weights('saved_models/model_vgg16.keras') 
+plot_train_history(history2)
+score2 = model_vgg16.evaluate(test_ds, verbose=1)
+score1 = [0.5, 0.8]
+
+print(f'Model 1 Vanilla Loss: {score1[0]}, Accuracy: {score1[1]*100}')
+print(f'Model 2 VGG16 Loss: {score2[0]}, Accuracy: {score2[1]*100}')
+Y_pred = model_vgg16.predict(test_ds)
+y_pred = np.argmax(Y_pred, axis=1)
+confusion_mtx = confusion_matrix(y_pred, test_ds.classes)
+f,ax = plt.subplots(figsize=(12, 12))
+sns.heatmap(confusion_mtx, annot=True,
+            linewidths=0.01,
+            linecolor="yellow",
+            fmt= '.1f',ax=ax,)
+sns.color_palette("rocket", as_cmap=True)
+
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+ax.xaxis.set_ticklabels(test_ds.class_indices)
+ax.yaxis.set_ticklabels(rice_classes)
+plt.title("Confusion Matrix")
+plt.show()
+report2 = classification_report(test_ds.classes, y_pred, target_names=rice_classes, output_dict=True)
+df2 = pd.DataFrame(report1).transpose()
+df2
+import numpy as np
+labels = np.array(rice_classes)
+label_map = {label: i for i, label in enumerate(np.unique(labels))}
+integer_labels = np.array([label_map[label] for label in labels])
+integer_labels = integer_labels[:, np.newaxis]
+integer_labels = np.eye(5)[integer_labels.ravel()]
+print(integer_labels)
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.layers import Input, Flatten, Dense
+from tensorflow.keras.models import Model
+img_width, img_height = 224, 224
+batch_size = 32
+num_classes = 5
+train_data_dir = '/content/drive/MyDrive/imgs/train'
+validation_data_dir = '/content/drive/MyDrive/imgs/val'
+test_data_dir = '/content/drive/MyDrive/imgs/test'
+train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+validation_datagen = ImageDataGenerator(rescale=1./255)
+test_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(
+        train_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+validation_generator = validation_datagen.flow_from_directory(
+        validation_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='categorical')
+
+test_generator = test_datagen.flow_from_directory(
+        test_data_dir,
+        target_size=(img_width, img_height),
+        batch_size=batch_size,
+        class_mode='categorical',
+        shuffle=False)
+vgg16_model = VGG16(weights='imagenet', include_top=False, input_shape=(img_width, img_height, 3))
+for layer in vgg16_model.layers:
+    layer.trainable = False
+input_layer = Input(shape=(img_width, img_height, 3))
+x = vgg16_model(input_layer)
+x = Flatten()(x)
+x = Dense(4096, activation='relu')(x)
+x = Dense(4096, activation='relu')(x)
+output_layer = Dense(num_classes, activation='softmax')(x)
+model = Model(inputs=input_layer, outputs=output_layer)
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+history = model.fit(
+        train_generator,
+        steps_per_epoch=train_generator.samples // batch_size,
+        epochs=10,
+        validation_data=validation_generator,
+        validation_steps=validation_generator.samples // batch_size)
+model.save("/content/drive/MyDrive/Model_Save/v16.h5")
+from keras.models import load_model
+classifier=load_model("/content/drive/MyDrive/Model_Save/v16.h5")
+preds = model.predict(test_generator, steps=test_generator.samples // batch_size, verbose=1)
+predicted_classes = tf.argmax(preds, axis=1)
+true_classes = test_generator.classes
+score = model.evaluate(test_generator, verbose=0)
+print(f'Test loss: {score[0]}')
+print(f'Test accuracy: {score[1]}')
+arr1d = np.array(rice_classes)
+arr2d = arr1d.reshape((1,-1))
+print(arr2d)
+plt.figure(figsize=(10, 10))
+x, label= train_ds.__next__()
+for i in range(9):
+    plt.subplot(3, 3, i+1)
+    plt.imshow(x[i])
+    result = np.where(label[i]==1)
+    predict = model_vgg16(tf.expand_dims(x[i], 0))
+    score = tf.nn.softmax(predict[0])
+    score_label = rice_classes[np.argmax(score)]
+    plt.title(f'Truth: {rice_classes[result[0][0]]}\nPrediction:{score_label}')
+    plt.axis(False)
