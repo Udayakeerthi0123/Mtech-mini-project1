6a6fac1e482a918bdc6796262a7d04b9908d6abf
Udayakeerthi0123 <114857285+Udayakeerthi0123@users.noreply.github.com>
2024-11-22 22:02:28 +0530

    dataset.txt

---
+++ dataset.py
@@ -0,0 +1,35 @@
+About Dataset
+Rice Image Dataset
+DATASET: https://www.muratkoklu.com/datasets/
+
+Citation Request: See the articles for more detailed information on the data.
+
+Koklu, M., Cinar, I., & Taspinar, Y. S. (2021). Classification of rice varieties with deep learning methods. Computers and Electronics in Agriculture, 187, 106285. https://doi.org/10.1016/j.compag.2021.106285
+
+Cinar, I., & Koklu, M. (2021). Determination of Effective and Specific Physical Features of Rice Varieties by Computer Vision In Exterior Quality Inspection. Selcuk Journal of Agriculture and Food Sciences, 35(3), 229-243. https://doi.org/10.15316/SJAFS.2021.252
+
+Cinar, I., & Koklu, M. (2022). Identification of Rice Varieties Using Machine Learning Algorithms. Journal of Agricultural Sciences https://doi.org/10.15832/ankutbd.862482
+
+Cinar, I., & Koklu, M. (2019). Classification of Rice Varieties Using Artificial Intelligence Methods. International Journal of Intelligent Systems and Applications in Engineering, 7(3), 188-194. https://doi.org/10.18201/ijisae.2019355381
+
+DATASET: https://www.muratkoklu.com/datasets/
+
+Highlights
+• Arborio, Basmati, Ipsala, Jasmine and Karacadag rice varieties were used.
+• The dataset (1) has 75K images including 15K pieces from each rice variety. The dataset (2) has 12 morphological, 4 shape and 90 color features.
+• ANN, DNN and CNN models were used to classify rice varieties.
+• Classified with an accuracy rate of 100% through the CNN model created.
+• The models used achieved successful results in the classification of rice varieties.
+
+In my project I have used these five types of rice  to classify by using techniques Vanilla CNN and Transfer Learning architeture VGG16.
+ 
+Abstract:
+India is the largest producer of rice and is one of the most important species cultivated. In the vast fields of villages, 
+towns and cities where rice is cultivated, numerous varieties of rice seeds are created to suit a range of environmental conditions 
+including climate, soil and water. Those seeds carry a broad range of health benefits and also taste. Apart from the new seeds released 
+in the market, there are now various techniques of multiple cropping. This paper proposes to classify the different varieties of 
+rice plants using Enhanced Transfer Learning on seedlings images. Predicting the type of rice seed has been attempted with existing 
+models, but the results showed low accuracy and consistence which indicates a need to improve. It consists of a specific model for 
+scanning rice seeds and classify it in order to help end users purchase the correct type of rice according to its benefits. The 
+developed model is trained on five categories of rice seeds and is found more trustworthy and accurate than the previous models 
+used for similar purposes. The project also explores the accuracy gap between Vanilla CNN and Transfer Learning architecture VGG16.
